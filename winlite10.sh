f02d9763e889a5b16b2128261304e7019f90fcc5
Jumbo810 <106257478+Jumbo810@users.noreply.github.com>
2022-06-10 10:08:27 +0500

    Update winlite10.sh

--- winlite10.sh
+++ winlite10.sh
@@ -1,7 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "27guGh9wLHiSNzsg92F0ziRYYq1_2HDJWEg4f65NANjUPbuDB" CRP
-./ngrok authtoken $CRP 
+./ngrok authtoken 27guGh9wLHiSNzsg92F0ziRYYq1_2HDJWEg4f65NANjUPbuDB 
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
 sudo apt update -y > /dev/null 2>&1
